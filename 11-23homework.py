484b0d1b7e1d6493beedc47e80464a6cd3819ca8
poweiiiii <4b0g0175@stust.edu.tw>
2023-11-23 11:57:05 +0800

    Update 11-23homework.py

--- 11-23homework.py
+++ 11-23homework.py
@@ -9,6 +9,7 @@ class Employee:
     def assign_department(self , new_department):
         self.department = new_department
 
+    #Print details of every employees
     def print_detail(self):
         print('{} {} {} {} '.format(self.name , self.id , self.salary , self.department))
 
@@ -19,12 +20,15 @@ class Employee:
             overtime = worktime - 50 
         self.salary = self.salary + (overtime * (self.salary / 50))
 
+
+#Give every emplyees their datas
 emp1 = Employee("ADAMS", "E7876", 50000, "ACCOUNTING" )
 emp2 = Employee("JONES", "E7499", 45000, "RESEARCH" )
 emp3 = Employee("MARTIN", "E7900", 50000, "SALES")
 emp4 = Employee("SMITH", "E7698", 55000, "OPERATIONS" )
 
-emp2.overtime_bonus(45000 ,63)
+#Employee2's and Employee4's worktime were over 50h , sp their salary will be change
+emp2.overtime_bonus(45000 ,56)
 emp4.overtime_bonus(55000 , 59)       
 
 emp2.assign_department('SALES')
